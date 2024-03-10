f4e7cc156c4f3f52111eacf24cf3d5be2d0cf583
LoreviQ <lorevi@protonmail.com>
2024-03-10 19:06:15 +0000

    Simple sebserver that serves index

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/LoreviQ/PrivateWebServer
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	port := "8080"
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/assets/logo.png", http.FileServer(http.Dir("./assets/logo.png")))
+	corsMux := middlewareCors(mux)
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: corsMux,
+	}
+	log.Printf("Serving on port: %s\n", port)
+	log.Fatal(server.ListenAndServe())
+}
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
