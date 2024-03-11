7f1ddffa59fe39babfde314f0539e7c26d18b8f5
LoreviQ <lorevi@protonmail.com>
2024-03-11 18:08:36 +0000

    Modified metrics to return html

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	cfg := apiConfig{fileserverHits: 0}
 	mux.Handle("/app/*", http.StripPrefix("/app", cfg.metricsIncMiddleware(http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("GET /api/healthz", healthzHandler)
-	mux.HandleFunc("GET /api/metrics", cfg.metricsReportingHandler)
+	mux.HandleFunc("GET /admin/metrics", cfg.metricsReportingHandler)
 	mux.HandleFunc("GET /api/reset", cfg.metricsResetHandler)
 
 	corsMux := corsMiddleware(mux)
