d001910166e7060278dacde2f3303e2cc772f17a
LoreviQ <lorevi@protonmail.com>
2024-03-13 15:43:15 +0000

    Moved database to internal package

--- main.go
+++ main.go
@@ -30,6 +30,8 @@ func initialiseServer(cfg apiConfig, mux *http.ServeMux) *http.Server {
 	mux.HandleFunc("POST /api/users", cfg.postUserHandler)
 	mux.HandleFunc("PUT /api/users", cfg.putUserHandler)
 	mux.HandleFunc("POST /api/login", cfg.postLoginHandler)
+	//mux.HandleFunc("POST /api/refresh", cfg.postRefreshHandler)
+	//mux.HandleFunc("POST /api/revoke", cfg.postRevokeHandler)
 
 	corsMux := corsMiddleware(mux)
 
